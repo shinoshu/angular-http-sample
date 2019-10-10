41c47fa40c7abb9a77cb733eefcc31f93c566cfa
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-10-10 16:04:02 +0900

    update user.service.ts

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
 
+import { ApiClientService } from './api-client.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  resourceName = 'user';
 
-  constructor() { }
+  constructor(private apiClientService: ApiClientService) {}
+
+  addUser(user: any) {
+    return this.apiClientService.create(this.resourceName, user).toPromise();
+  }
+
+  getUser(id: string) {
+    return this.apiClientService.get(this.resourceName, id).toPromise();
+  }
+
+  getUserAll() {
+    return this.apiClientService.list(this.resourceName).toPromise();
+  }
+
+  updateUser(user: any) {
+    return this.apiClientService.update(this.resourceName, user.id, user).toPromise();
+  }
+
+  deleteUser(id: string) {
+    return this.apiClientService.delete(this.resourceName, id).toPromise();
+  }
 }
