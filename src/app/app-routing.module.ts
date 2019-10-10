31f62a4f5ddbab41a754179c9188b5d1ddb67381
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-10-10 20:57:11 +0900

    update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormComponent } from './form/form.component';
+import { TableComponent } from './table/table.component';
+import { TreeComponent } from './tree/tree.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'form', component: FormComponent },
+  { path: 'table', component: TableComponent },
+  { path: 'tree', component: TreeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
